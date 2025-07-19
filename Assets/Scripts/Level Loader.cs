ddb462e08929e4c4d5170b6eb8ddb97f4f1b3f06
Copy <theofficialxuan@gmail.com>
2025-07-18 18:03:08 -0700

    got levels to work Need to fix interaction with if you are in or out the area

--- Assets/Scripts/Level Loader.cs
+++ Assets/Scripts/Level Loader.cs
@@ -3,13 +3,37 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    public string sceneToLoad;
+    private int playersInRange = 0;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (playersInRange > 0 && Input.GetKeyDown(KeyCode.E))
         {
-            SceneManager.LoadScene(sceneToLoad);
+            LoadNextLevel();
+        }
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playersInRange++;
+            Debug.Log("Player entered range.");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playersInRange = Mathf.Max(playersInRange - 1, 0);
+            Debug.Log("Player left range.");
         }
     }
 }
